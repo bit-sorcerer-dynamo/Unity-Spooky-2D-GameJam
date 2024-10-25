38ba329e085c8230b5a1090858901518f47b40e0
LateX1803 <bit.sorcerer.dynamo@gmail.com>
2024-10-25 23:44:00 +0530

    Got Some Things Done!
    I have the Basic Player Movement and a very very basic layout of the game... less see if this turns out well....

---
+++ Spooky 2D Game Jam/Assets/Scripts/Gameplay/Health.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float maxHealth = 100f;
+
+    private float currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;    
+    }
+
+    void TakeDamage(float damage)
+    {
+        if (currentHealth - damage > 0) currentHealth -= damage;
+        else currentHealth = 0;
+    }
+}
